ced79fb57e437506cd5fe48320b56fc656b32d33
lukasjaronis <jaronisluke13@gmail.com>
2019-11-11 15:25:08 -0500

    fixed router location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
-  <Router>
     <Navigation />
-  </Router>
   </div>
 );
 
